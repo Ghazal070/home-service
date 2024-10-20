7accf6087e2c5a6ad92a13911b8e93b005d6ec3c
ghazal <ghazaltorabi020@gmail.com>
2024-10-20 16:18:39 +0330

    fix score in search api

--- src/main/java/application/service/impl/UserSpecificationImpl.java
+++ src/main/java/application/service/impl/UserSpecificationImpl.java
@@ -26,129 +26,96 @@ public class UserSpecificationImpl<T extends Users> implements UserSpecification
     private final CustomerRepository customerRepository;
     private final ExpertRepository expertRepository;
 
-    @Override
-    public void validate(SearchDto searchDto) {
+    @Override public void validate(SearchDto searchDto) {
         if (searchDto.getUserRole() != null && !(searchDto.getUserRole().equals("Customer") || searchDto.getUserRole().equals("Expert"))) {
             throw new ValidationException("UserType must be Customer or Expert");
         }
         if (searchDto.getUserRole() != null && searchDto.getUserRole().equals("Customer")) {
             if (searchDto.getDutyTitle() != null || searchDto.getMinScore() != null || searchDto.getMaxScore() != null) {
-                throw new ValidationException("Customer dont have score or duty title");
+                throw new ValidationException("Customer don't have score or duty title");
             }
         }
     }
 
-    @Override
-    public List findAllBySearchDto(SearchDto searchDto) {
+    @Override public List findAllBySearchDto(SearchDto searchDto) {
         validate(searchDto);
         if (searchDto.getUserRole() != null) {
-            switch (searchDto.getUserRole()) {
-                case "Customer": {
-                    return customerRepository.findAll(
-                            (root, query, cb) -> {
-                                List<Predicate> predicates = new ArrayList<>();
-                                fillFirstName(predicates, root, cb, searchDto.getFirstName());
-                                fillLastName(predicates, root, cb, searchDto.getLastName());
-                                fillEmail(predicates, root, cb, searchDto.getEmail());
-                                return cb.and(predicates.toArray(new Predicate[0]));
-
-                            }
-                    );
-
-                }
-                //todo duplicate base data and 53 line error// return switch
-                case "Expert" : {
-                    return expertRepository.findAll(
-                            (root, query, cb) -> {
-                                List<Predicate> predicates = new ArrayList<>();
-                                fillFirstName(predicates, root, cb, searchDto.getFirstName());
-                                fillLastName(predicates, root, cb, searchDto.getLastName());
-                                fillEmail(predicates, root, cb, searchDto.getEmail());
-                                fillDutyTitle(predicates, root, cb, searchDto.getDutyTitle());
-                                fillMinScore(predicates, root, cb, searchDto.getMinScore(), searchDto.getMaxScore());
-                                return cb.and(predicates.toArray(new Predicate[0]));
-
-                            }
-                    );
-                }
-
-                default: throw new ValidationException("User role can only be Customer or Expert or null");
-            }
+            return handleSpecificUserRole(searchDto);
         } else {
-            return userRepository.findAll(
-                    (root, query, cb) -> {
-                        List<Predicate> predicates = new ArrayList<>();
-                        fillFirstName(predicates, root, cb, searchDto.getFirstName());
-                        fillLastName(predicates, root, cb, searchDto.getLastName());
-                        fillEmail(predicates, root, cb, searchDto.getEmail());
-                        fillDutyTitle(predicates, root, cb, searchDto.getDutyTitle());
-                        fillMinScore(predicates, root, cb, searchDto.getMinScore(), searchDto.getMaxScore());
-                        cb.and(predicates.toArray(new Predicate[0]));
-
-                        return cb.and(predicates.toArray(new Predicate[0]));
-
-                    }
-            );
+            return handleGenericUserSearch(searchDto, userRepository);
         }
-
     }
 
-    private void fillFirstName(List<Predicate> predicates, Root root
-            , CriteriaBuilder cb, String firstName) {
+    private List handleSpecificUserRole(SearchDto searchDto) {
+        if (searchDto.getUserRole().equals("Customer")) {
+            return customerRepository.findAll((root, query, cb) -> {
+                List<Predicate> predicates = new ArrayList<>();
+                populateCommonPredicates(predicates, root, cb, searchDto);
+                return cb.and(predicates.toArray(new Predicate[0]));
+            });
+        } else if (searchDto.getUserRole().equals("Expert")) {
+            return expertRepository.findAll((root, query, cb) -> {
+                List<Predicate> predicates = new ArrayList<>();
+                populateCommonPredicates(predicates, root, cb, searchDto);
+                fillDutyTitle(predicates, root, cb, searchDto.getDutyTitle());
+                fillMinScore(predicates, root, cb, searchDto.getMinScore(), searchDto.getMaxScore());
+                return cb.and(predicates.toArray(new Predicate[0]));
+            });
+        } else {
+            throw new ValidationException("User role can only be Customer or Expert or null");
+        }
+    }
+//done edit duplicate code
+    private List handleGenericUserSearch(SearchDto searchDto, UserRepository<T> userRepository) {
+        return userRepository.findAll((root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            populateCommonPredicates(predicates, root, cb, searchDto);
+            fillDutyTitle(predicates, root, cb, searchDto.getDutyTitle());
+            fillMinScore(predicates, root, cb, searchDto.getMinScore(), searchDto.getMaxScore());
+            return cb.and(predicates.toArray(new Predicate[0]));
+        });
+    }
+
+    private void populateCommonPredicates(List<Predicate> predicates, Root root, CriteriaBuilder cb, SearchDto searchDto) {
+        fillFirstName(predicates, root, cb, searchDto.getFirstName());
+        fillLastName(predicates, root, cb, searchDto.getLastName());
+        fillEmail(predicates, root, cb, searchDto.getEmail());
+    }
+
+    private void fillFirstName(List<Predicate> predicates, Root root, CriteriaBuilder cb, String firstName) {
         if (StringUtils.isNotBlank(firstName)) {
-            predicates.add(
-                    cb.like(
-                            root.get(Users_.FIRST_NAME),
-                            "%" + firstName + "%"
-                    )
-            );
+            predicates.add(cb.like(root.get(Users_.FIRST_NAME), "%" + firstName + "%"));
         }
     }
 
-    private void fillLastName(List<Predicate> predicates, Root root
-            , CriteriaBuilder cb, String lastName) {
+    private void fillLastName(List<Predicate> predicates, Root root, CriteriaBuilder cb, String lastName) {
         if (StringUtils.isNotBlank(lastName)) {
-            predicates.add(
-                    cb.like(
-                            root.get(Users_.LAST_NAME),
-                            "%" + lastName + "%"
-                    )
-            );
+            predicates.add(cb.like(root.get(Users_.LAST_NAME), "%" + lastName + "%"));
         }
     }
 
-    private void fillEmail(List<Predicate> predicates, Root root
-            , CriteriaBuilder cb, String email) {
+    private void fillEmail(List<Predicate> predicates, Root root, CriteriaBuilder cb, String email) {
         if (StringUtils.isNotBlank(email)) {
-            predicates.add(
-                    cb.like(
-                            root.get(Users_.PROFILE).get(Profile_.EMAIL),
-                            "%" + email + "%"
-                    )
-            );
+            predicates.add(cb.like(root.get(Users_.PROFILE).get(Profile_.EMAIL), "%" + email + "%"));
         }
     }
 
-    private void fillDutyTitle(List<Predicate> predicates, Root root
-            , CriteriaBuilder cb, String dutyTitle) {
+    private void fillDutyTitle(List<Predicate> predicates, Root root, CriteriaBuilder cb, String dutyTitle) {
         if (StringUtils.isNotBlank(dutyTitle)) {
-            predicates.add(
-                    cb.like(
-                            root.join(Expert_.DUTIES).get(Duty_.TITLE),
-                            "%" + dutyTitle + "%"
-                    )
-            );
+            predicates.add(cb.like(root.join(Expert_.DUTIES).get(Duty_.TITLE), "%" + dutyTitle + "%"));
         }
     }
 
     private void fillMinScore(List<Predicate> predicates, Root root, CriteriaBuilder cb, Integer minScore, Integer maxScore) {
         if (minScore != null && maxScore != null) {
-            predicates.add(
-                    cb.between(
-                            root.get(Expert_.SCORE), minScore, maxScore
-                    )
-            );
+            predicates.add(cb.between(root.get(Expert_.SCORE), minScore, maxScore));
+        }
+        else if (minScore != null) {
+            predicates.add(cb.between(root.get(Expert_.SCORE), minScore,Integer.MAX_VALUE));
+        }
+        else if (maxScore != null) {
+            predicates.add(cb.between(root.get(Expert_.SCORE), Integer.MIN_VALUE,maxScore));
         }
     }
-
+//done handle min Score , infighting
 }
