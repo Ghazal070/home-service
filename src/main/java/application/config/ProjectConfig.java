79a2ab8246d30496ac191664c0d32d9f249628bb
ghazal <ghazaltorabi070@gmail.com>
2024-10-22 21:28:16 +0330

    fix preAuthorize for duties

--- src/main/java/application/config/ProjectConfig.java
+++ src/main/java/application/config/ProjectConfig.java
@@ -6,7 +6,9 @@ import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,7 +18,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import java.util.Collection;
 
 @Configuration
-
+@EnableMethodSecurity
+@EnableWebSecurity
 public class ProjectConfig {
 
 
