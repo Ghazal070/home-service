9e8ea29a038fc766dac92444198c817f9fb4e481
ghazal <ghazaltorabi020@gmail.com>
2024-11-02 08:45:52 +0330

    fix jwt some error

--- src/main/java/application/config/jwt/JwtService.java
+++ src/main/java/application/config/jwt/JwtService.java
@@ -3,17 +3,12 @@ package application.config.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,13 +17,12 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private String secretKey = "";
+    private final SecretKey secretKey;
 
     public JwtService() {
         try {
             KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-            SecretKey sk = keyGen.generateKey();
-            secretKey=Base64.getEncoder().encodeToString(sk.getEncoded());
+            this.secretKey = keyGen.generateKey();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
@@ -50,8 +44,7 @@ public class JwtService {
     }
 
     private SecretKey getKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
+        return secretKey;
     }
 
     public String extractUsername(String token) {
